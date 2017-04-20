ceaa5e821df160658e9df3b234db37cc99903860
Séverin Bruhat <sbruhat@gmail.com>
2017-04-20 12:02:09 +0000

    finish master template implementation

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->page = "welcome_message";
+		$this->layout();
+		//http://vtube.ge/en/videos/play/ZlY5Y3QzTVYxZEk
 	}
 }
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * MY Controller class
+ */
+class MY_Controller extends CI_Controller
+{
+    protected $data = [];
+
+    /**
+     * [__construct description]
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * handle a master layout
+     * @return [type] [description]
+     */
+    public function layout()
+    {
+        $this->template['header'] = $this->load->view('templates/header', $this->data, true);
+        $this->template['footer'] = $this->load->view('templates/footer', $this->data, true);
+        $this->template['page'] = $this->load->view($this->page, $this->data, true);
+        $this->load->view('templates/main', $this->template);
+    }
+}
